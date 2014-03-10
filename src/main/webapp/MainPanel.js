c02b744b1be29ad38b0feea2ef3f5771e50bfed6
shightower <ricohightower@gmail.com>
2014-03-10 17:29:28 -0400

    main panel
    initial main panel

---
+++ src/main/webapp/MainPanel.js
@@ -0,0 +1,40 @@
+Ext.define('Cupboard.MainPanel', {
+  extend: 'Ext.panel.Panel',
+  alias: 'widget.mainpanel',
+  layout: 'card',
+  region: 'center',
+  border: false,
+  bodyBorder: false,
+  bodyPadding: false,
+  padding: false,
+  tbar: {
+    layout: {
+      type: 'hbox',
+      pack: 'center'
+    },
+    defaults: {
+      cls: 'boldText'
+    },
+    items: [{
+      xtype: 'button',
+      text: Cupboard.label.overview,
+      handler: function() {
+        var panel = this.up('panel');
+        panel.getLayout().setActiveItem(0);
+      }
+    },{
+      xtype: 'button',
+      text: Cupboard.label.createUser,
+      handler: function() {
+        var panel = this.up('panel');
+        panel.getLayout().setActiveItem(1);
+      }
+    }]
+  }, //end tbar
+  items: [{
+    xtype: 'overviewpanel'
+  },{
+    xtype: 'creteUserPanel'
+  }]
+
+});
